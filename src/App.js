50137ce117fcfb30f3db58b50afb703aeff102c8
Julian Weise <privat@julianweise.de>
2017-07-18 14:14:13 +0200

    Integrated Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+global.jQuery = require('jquery');
+global.Tether = require('tether');
+require('bootstrap');
+require('bootstrap/dist/css/bootstrap.css');
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="container-fluid">
+            <h1>Welcome</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
     );
   }
 }
