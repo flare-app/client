6717330db3a06c75edf42e2fb70e03e2e3f3c6d0
Christian Friedow <christian@friedow.com>
2017-07-18 16:15:07 +0200

    Designed response page

--- src/Response/Response.js
+++ src/Response/Response.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import icon from '../resources/app-icon.svg';
 
 class Response extends Component {
   render() {
     return (
-        <div>
-            <h1>Response</h1>
+        <div className="d-flex flex-column justify-content-center vh-100">
+            <div className="d-flex justify-content-center mb-5">
+                <img src={icon} className="app-icon" alt="icon" />
+            </div>
+            <div className="d-flex justify-content-center mb-5">
+                <div className="btn-group btn-group-lg" role="group">
+                    <button type="button" className="btn btn-success">
+                        <i className="fa fa-bicycle"></i>
+                    </button>
+                    <button type="button" className="btn btn-success">
+                        <i className="fa fa-car"></i>
+                    </button>
+                    <button type="button" className="btn btn-success">
+                        <i className="fa fa-bus"></i>
+                    </button>
+                    <button type="button" className="btn btn-success">
+                        <i className="fa fa-rocket"></i>
+                    </button>
+                </div>
+            </div>
+            <div className="d-flex justify-content-center mb-5">
+                <div className="btn-group btn-group-lg" role="group">
+                    <button type="button" className="btn btn-danger">
+                        Kein Bock. Couch.
+                    </button>
+                </div>
+            </div>
         </div>
     );
   }
