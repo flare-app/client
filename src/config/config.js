44ed5f89f473412463a5dfdb6a26bb26cf83313e
Christian Friedow <christian@friedow.com>
2017-07-20 15:43:55 +0200

    Added transportation mode config

---
+++ src/config/config.js
@@ -0,0 +1,20 @@
+export default {
+	transportationModes: [
+		{
+			name: 'Foot',
+			icon: 'fa-blind',
+		},
+		{
+			name: 'Bike',
+			icon: 'fa-bicycle',
+		},
+		{
+			name: 'Car',
+			icon: 'fa-car',
+		},
+		{
+			name: 'Bus',
+			icon: 'fa-bus',
+		},
+	],
+};
