e4646be5df9278a56d344909144f68e21b548f9e
Julian Weise <privat@julianweise.de>
2017-07-25 12:37:26 +0200

    Added ServiceWorker to Response-UI

--- src/Response/Response.js
+++ src/Response/Response.js
@@ -3,16 +3,45 @@ import icon from '../resources/app-icon.svg';
 
 class Response extends Component {
 	
+	constructor(props) {
+		super(props);
+		this.onSubmitResponse = this.onSubmitResponse.bind(this);
+		this.onSubmitMistake = this.onSubmitMistake.bind(this);
+		this.state = {dataSended: false};
+	}
+
+	onSubmitMistake() {
+		if ('serviceWorker' in navigator) {
+			const response = {
+				authToke: this.props.authToken,
+			};
+			this.sendData('mistake', response);
+			this.setState({dataSended: false});
+		}
+	}
+	
 	onSubmitResponse(willAppear, transportation) {
 		if ('serviceWorker' in navigator) {
 			const response = {
 				willAppear: willAppear,
-				transportation: transportation
+				transportation: transportation,
+				authToke: this.props.authToken,
+				geoLocation: window.localStorage.geoLocation
 			};
-			navigator.serviceWorker.ready.then(function() {
-				navigator.serviceWorker.controller.postMessage({type: 'response', response: response});
-			});
+			this.sendData('response', response);
+			navigator.geolocation.getCurrentPosition(function (position) {
+				const crd = position.coords;
+				response.geolocation = {lat: crd.latitude, long: crd.longitude};
+				this.sendData('response', response);
+			}.bind(this));
+			this.setState({dataSended: true});
 		}
+	}
+	
+	sendData(type, response) {
+		navigator.serviceWorker.ready.then(function() {
+			navigator.serviceWorker.controller.postMessage({type: type, response: response});
+		});
 	}
 
 	render() {
@@ -21,39 +50,54 @@ class Response extends Component {
 				<div className="d-flex justify-content-center mb-5">
 					<img src={icon} className="app-icon" alt="icon"/>
 				</div>
-				<div className="d-flex justify-content-center mb-5">
-					<div className="btn-group btn-group-lg" role="group">
-						<button 
-							type="button" className="btn btn-success" 
-							onClick={this.onSubmitResponse.bind(this, true, 'walk')}>
-							<i className="fa fa-blind fa-walk"/>
-						</button>
-						<button 
-							type="button" className="btn btn-success"
-							onClick={this.onSubmitResponse.bind(this, true, 'bike')}>
-							<i className="fa fa-bicycle"/>
-						</button>
-						<button 
-							type="button" className="btn btn-success"
-							onClick={this.onSubmitResponse.bind(this, true, 'car')}>
-							<i className="fa fa-car"/>
-						</button>
-						<button 
-							type="button" className="btn btn-success"
-							onClick={this.onSubmitResponse.bind(this, true, 'publicTransport')}>
-							<i className="fa fa-bus"/>
-						</button>
+				{!this.state.dataSended &&
+					<div>
+						<div className="d-flex justify-content-center mb-5">
+							<div className="btn-group btn-group-lg" role="group">
+								<button
+									type="button" className="btn btn-success"
+									onClick={this.onSubmitResponse.bind(this, true, 'walk')}>
+									<i className="fa fa-blind fa-walk"/>
+								</button>
+								<button
+									type="button" className="btn btn-success"
+									onClick={this.onSubmitResponse.bind(this, true, 'bike')}>
+									<i className="fa fa-bicycle"/>
+								</button>
+								<button
+									type="button" className="btn btn-success"
+									onClick={this.onSubmitResponse.bind(this, true, 'car')}>
+									<i className="fa fa-car"/>
+								</button>
+								<button
+									type="button" className="btn btn-success"
+									onClick={this.onSubmitResponse.bind(this, true, 'publicTransport')}>
+									<i className="fa fa-bus"/>
+								</button>
+							</div>
+						</div>
+						<div className="d-flex justify-content-center mb-5">
+							<div className="btn-group btn-group-lg" role="group">
+								<button
+									type="button" className="btn btn-danger"
+									onClick={this.onSubmitResponse.bind(this, false, 'none')}>
+									<i className="no-icon fa fa-times"/>
+								</button>
+							</div>
+						</div>
 					</div>
-				</div>
-				<div className="d-flex justify-content-center mb-5">
-					<div className="btn-group btn-group-lg" role="group">
-						<button 
-							type="button" className="btn btn-danger"
-							onClick={this.onSubmitResponse.bind(this, false, 'none')}>
-							<i className="no-icon fa fa-times"/>
-						</button>
+				}
+				{this.state.dataSended &&
+					<div className="d-flex justify-content-center mb-5">
+						<div className="btn-group btn-group-lg" role="group">
+							<button
+								type="button" className="btn btn-warning"
+								onClick={this.onSubmitMistake}>
+								Ooops
+							</button>
+						</div>
 					</div>
-				</div>
+				}
 			</div>
 		);
 	}
